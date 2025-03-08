0594a67c5fcb3b868f744542e79581e8a05f6473
Mohammed Aatef <moatif1416@gmail.com>
2025-03-08 14:40:30 +0530

    v1.0

---
+++ ec2-terra/aws.main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = var.region
+}
+
+#EC2 Instance 
+resource "aws_instance" "EC2" {
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  key_name = var.key_name
+  vpc_security_group_ids = length(var.security_groups) > 0 ? [var.security_groups] : null
+  
+  private_ip = var.private_ip != "" ? var.private_ip : null
+  iam_instance_profile = var.iam_instance_profile
+  
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp3"
+  }
+
+  #ebs_block_device {
+   # device_name = "/dev/sdb"
+    #volume_size = var.extra_volume_size
+    #volume_type = "gp3"
+  #}
+
+  tags = {
+    Name = var.Name 
+  }
+
+  
+  
+}
+
