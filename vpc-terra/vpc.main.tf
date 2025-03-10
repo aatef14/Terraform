4ec769914b3246a8d6d96495e97d5c60d499f4c8
Mohammed Aatef <moatif1416@gmail.com>
2025-03-11 01:21:50 +0530

    v0.3

--- vpc-terra/vpc.main.tf
+++ vpc-terra/vpc.main.tf
@@ -29,3 +29,53 @@ resource "aws_subnet" "Public1a" {
     availability_zone = var.az1a
   
 }
+
+resource "aws_subnet" "Private1a" {
+    tags = {
+        Name = "pvt1a"
+    }
+    vpc_id = aws_vpc.myVpc.id
+    cidr_block = local.cidr_block_pvt1a
+    availability_zone = var.az1a
+  
+}
+
+resource "aws_subnet" "Public1b" {
+    tags = {
+        Name = "pub1b"
+    }
+    vpc_id = aws_vpc.myVpc.id
+    cidr_block = local.cidr_block_pub1b
+    availability_zone = var.az1a
+  
+}
+
+resource "aws_subnet" "Private1b" {
+    tags = {
+        Name = "pvt1b"
+    }
+    vpc_id = aws_vpc.myVpc.id
+    cidr_block = local.cidr_block_pvt1b
+    availability_zone = var.az1a
+  
+}
+
+resource "aws_subnet" "Public1c" {
+    tags = {
+        Name = "pub1c"
+    }
+    vpc_id = aws_vpc.myVpc.id
+    cidr_block = local.cidr_block_pub1c
+    availability_zone = var.az1a
+  
+}
+
+resource "aws_subnet" "Private1c" {
+    tags = {
+        Name = "pvt1c"
+    }
+    vpc_id = aws_vpc.myVpc.id
+    cidr_block = local.cidr_block_pvt1c
+    availability_zone = var.az1a
+  
+}
